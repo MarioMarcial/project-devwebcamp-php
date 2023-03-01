3c88194df20b4287162b2bd18b5d2732f7ba6dec
Mario Marcial <mariomarciallp@gmail.com>
2023-02-28 20:30:27 -0600

    676.- Autenticación, registro, confirmar cuenta & olvidé password - Añadiendo CSS para el formulario

--- views/auth/login.php
+++ views/auth/login.php
@@ -3,11 +3,11 @@
   <p class="auth__description">Inicia sesión en DevWebCamp</p>
   <form action="" class="form" method="POST">
     <div class="form__group">
-      <label for="email" class="form__group">Email:</label>
+      <label for="email" class="form__label">Email:</label>
       <input type="email" class="form__input" name="email" id="email" placeholder="Tu Email">
     </div>
     <div class="form__group">
-      <label for="password" class="form__group">Password:</label>
+      <label for="password" class="form__label">Password:</label>
       <input type="password" class="form__input" name="password" id="password" placeholder="Tu Password">
     </div>
     <input type="submit" class="form__submit" value="Iniciar Sesión">
