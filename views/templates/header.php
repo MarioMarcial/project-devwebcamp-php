3f55d7493f6572af30a495a0ae87a8eb35f24cc2
Mario Marcial <mariomarciallp@gmail.com>
2023-03-01 12:44:08 -0600

    680.- Creando el header - Añadiendo el HTML del header

---
+++ views/templates/header.php
@@ -0,0 +1,18 @@
+<header class="header">
+  <div class="header__container">
+    <nav class="header__nav">
+      <a href="/registro" class="header__link">Registro</a>
+      <a href="/login" class="header__link">Iniciar Sesión</a>
+    </nav>
+    <div class="header__content">
+      <a href="/">
+        <h1 class="header__logo">
+        &#60;DevWebCamp/>
+        </h1>
+        <p class="header__text">Octubre 5-6 - 2023</p>
+        <p class="header__text header__text--mode">En Línea - Presencial</p>
+        <a href="/registro" class="header__button">Comprar Pase</a>
+      </a>
+    </div>
+  </div>
+</header>
