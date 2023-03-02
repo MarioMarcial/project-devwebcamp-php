1378d456a18cc1dc8c81f321c368ef5ca6a26aee
Mario Marcial <mariomarciallp@gmail.com>
2023-03-02 13:57:14 -0600

    686.- Creando el footer - Creando el HTML para el footer

---
+++ views/templates/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+  <div class="footer__grid">
+    <div class="footer__content">
+      <h3 class="footer__logo">
+        &#60;DevWebCamp/>
+      </h3>
+      <p class="footer__text">
+        DevWebCamp es una conferencia para desarrolladores de todos los niveles, se lleva a cabo de forma presencial o en línea.
+      </p>
+    </div>
+    <nav class="socials-menu">
+      <a class="socials-menu__link" rel="noopener noreferrer" target="_blank" href="https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628">
+        <span class="socials-menu__hide">Facebook</span>
+      </a> 
+      <a class="socials-menu__link" rel="noopener noreferrer" target="_blank" href="https://twitter.com/codigoconjuan">
+        <span class="socials-menu__hide">Twitter</span>
+      </a> 
+      <a class="socials-menu__link" rel="noopener noreferrer" target="_blank" href="https://youtube.com/codigoconjuan">
+        <span class="socials-menu__hide">YouTube</span>
+      </a> 
+      <a class="socials-menu__link" rel="noopener noreferrer" target="_blank" href="https://instagram.com/codigoconjuan">
+        <span class="socials-menu__hide">Instagram</span>
+      </a> 
+      <a class="socials-menu__link" rel="noopener noreferrer" target="_blank" href="https://tiktok.com/@codigoconjuan">
+        <span class="socials-menu__hide">Tiktok</span>
+      </a> 
+      <a class="socials-menu__link" rel="noopener noreferrer" target="_blank" href="https://github.com/codigoconjuan">
+        <span class="socials-menu__hide">GitHub</span>
+      </a>
+    </nav>
+  </div>
+  <div class="footer__copyright">
+    DevWebCamp
+    <span class="footer__copyright--regular">
+      Todos los derechos reservados <?php echo date('Y');?>
+    </span>
+  </div>
+</footer>
