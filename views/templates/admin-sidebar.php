2f46f4dd460ffee0905739b35b3ae9da7bcd671e
Mario Marcial <mariomarciallp@gmail.com>
2023-03-06 12:13:22 -0600

    697.- Creando un panel de administración - CSS al panel de administración

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -1,31 +1,31 @@
 <aside class="dashboard__sidebar">
   <nav class="dashboard__menu">
     <a href="/admin/dashboard" class="dashboard__link">
-      <i class="fa-solid fa-house"></i>
+      <i class="fa-solid fa-house dashboard__icon"></i>
       <span class="dashboard__menu-text">
         Inicio
       </span>
     </a>
     <a href="/admin/ponentes" class="dashboard__link">
-      <i class="fa-solid fa-microphone"></i>
+      <i class="fa-solid fa-microphone dashboard__icon"></i>
       <span class="dashboard__menu-text">
         Ponentes
       </span>
     </a>
     <a href="/admin/eventos" class="dashboard__link">
-      <i class="fa-solid fa-calendar"></i>
+      <i class="fa-solid fa-calendar dashboard__icon"></i>
       <span class="dashboard__menu-text">
         Eventos
       </span>
     </a>
     <a href="/admin/registrados" class="dashboard__link">
-      <i class="fa-solid fa-users"></i>
+      <i class="fa-solid fa-users dashboard__icon"></i>
       <span class="dashboard__menu-text">
         Registrados
       </span>
     </a>
     <a href="/admin/regalos" class="dashboard__link">
-      <i class="fa-solid fa-gift"></i>
+      <i class="fa-solid fa-gift dashboard__icon"></i>
       <span class="dashboard__menu-text">
         Regalos
       </span>
