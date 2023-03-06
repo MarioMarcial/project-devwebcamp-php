f78b0fad7c91b7e07905b249b4755c96f1e6e9a0
Mario Marcial <mariomarciallp@gmail.com>
2023-03-06 11:53:51 -0600

    696.- Creando un panel de administración - Creando los enlaces para el panel de administración

---
+++ views/templates/admin-sidebar.php
@@ -0,0 +1,34 @@
+<aside class="dashboard__sidebar">
+  <nav class="dashboard__menu">
+    <a href="/admin/dashboard" class="dashboard__link">
+      <i class="fa-solid fa-house"></i>
+      <span class="dashboard__menu-text">
+        Inicio
+      </span>
+    </a>
+    <a href="/admin/ponentes" class="dashboard__link">
+      <i class="fa-solid fa-microphone"></i>
+      <span class="dashboard__menu-text">
+        Ponentes
+      </span>
+    </a>
+    <a href="/admin/eventos" class="dashboard__link">
+      <i class="fa-solid fa-calendar"></i>
+      <span class="dashboard__menu-text">
+        Eventos
+      </span>
+    </a>
+    <a href="/admin/registrados" class="dashboard__link">
+      <i class="fa-solid fa-users"></i>
+      <span class="dashboard__menu-text">
+        Registrados
+      </span>
+    </a>
+    <a href="/admin/regalos" class="dashboard__link">
+      <i class="fa-solid fa-gift"></i>
+      <span class="dashboard__menu-text">
+        Regalos
+      </span>
+    </a>
+  </nav>
+</aside>
