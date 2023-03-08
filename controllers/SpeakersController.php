078b83c1a616bfa46fe1dd82bbdff2af9228c681
Mario Marcial <mariomarciallp@gmail.com>
2023-03-07 21:46:24 -0600

    709.- Modelo y base de datos para ponentes - Leer la imagen del ponente y generar versión png y webp

--- controllers/SpeakersController.php
+++ controllers/SpeakersController.php
@@ -1,8 +1,9 @@
 <?php
 namespace Controllers;
 
-use Model\Speaker;
 use MVC\Router;
+use Model\Speaker;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class SpeakersController {
   public static function index(Router $router) {
@@ -16,6 +17,23 @@ class SpeakersController {
     $alerts = [];
     $speaker = new Speaker;
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+      // Read image
+      if(!empty($_FILES['image']['tmp_name'])) {
+        $image_folder = '../public/img/speakers';
+        // Create folder if it dosn't exist
+        if(!is_dir($image_folder)) {
+          mkdir($image_folder, 0755, true);
+        }
+
+        $png_image = Image::make($_FILES['image']['tmp_name']->fit(800,800)->encode('png', 80));
+        $webp_image = Image::make($_FILES['image']['tmp_name']->fit(800,800)->encode('webp', 80));
+
+        $name_image = md5(uniqid(rand(), true));
+
+        $_POST['image'] = $name_image;
+      } 
+
       $speaker->sync();
 
       // validate
