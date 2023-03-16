4eee8d880426da5d8de92518b1e24392a60f2d06
Mario Marcial <mariomarciallp@gmail.com>
2023-03-16 11:55:14 -0600

    759.- Creando las páginas públicas frontend - Creando la página de workshops, conferencias y primeros pasos con CSS

--- views/pages/conferences.php
+++ views/pages/conferences.php
@@ -1,1 +1,28 @@
-<h2 class="dashboard__heading"><?php echo $title; ?></h2>
+<main class="diary">
+  <h2 class="diary__heading"><?php echo $title; ?></h2>
+  <p class="diary__description">Talleres y Conferencias dictados por expertos en Desarrollor Web</p>
+  <div class="events">
+    <h3 class="events__heading">&lt; Conferencias/></h3>
+    <p class="events__date">Viernes 5 de Octubre</p>
+    <div class="events__list">
+
+    </div>
+    <p class="events__date">Sábado 6 de Octubre</p>
+    <div class="events__list">
+
+    </div>
+
+  </div>
+  <div class="events events--workshops">
+    <h3 class="events__heading">&lt; WorkShops/></h3>
+    <p class="events__date">Viernes 5 de Octubre</p>
+    <div class="events__list">
+
+    </div>
+    <p class="events__date">Sábado 6 de Octubre</p>
+    <div class="events__list">
+
+    </div>
+
+  </div>
+</main>
