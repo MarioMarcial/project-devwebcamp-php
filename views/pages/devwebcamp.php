83f069d4ca4a1b91395fd3e43d5bf5797b1a4c7f
Mario Marcial <mariomarciallp@gmail.com>
2023-03-16 11:03:30 -0600

    757.- Creando las páginas públicas frontend - Creando la página sobre devwebcamp

--- views/pages/devwebcamp.php
+++ views/pages/devwebcamp.php
@@ -1,1 +1,25 @@
-<h2 class="dashboard__heading"><?php echo $title; ?></h2>
+<main class="devwebcamp">
+  <h2 class="devwebcamp__heading">
+    <?php echo $title; ?>
+  </h2>
+  <p class="devwebcamp__description">
+    Conoce la conferencia más importante de Latinoamérica
+  </p>
+  <div class="devwebcamp__grid">
+    <div class="devwebcamp__image">
+      <picture>
+        <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+        <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/sobre_devwebcamp.jpg" alt="Imagen DevWebCamp">
+      </picture>
+    </div>
+    <div class="devwebcamp__content">
+      <p class="devwebcamp__text">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint inventore totam velit ut quos dignissimos dolorum earum voluptatem, a incidunt nostrum consectetur dolorem sapiente odit eaque quibusdam sequi nemo? Officiis?
+      </p>
+      <p class="devwebcamp__text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quos fuga autem ratione voluptatibus quae aut accusamus aperiam, eius nemo blanditiis? Nesciunt unde nostrum quam.
+      </p>
+    </div>
+  </div>
+</main>
