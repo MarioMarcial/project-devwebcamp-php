04e717f16bd83e500c0b29a84b2b5364d4bcd13c
Mario Marcial <mariomarciallp@gmail.com>
2023-03-16 12:58:49 -0600

    762.- Creando las páginas públicas frontend - Aplicando CSS a los eventos

--- views/pages/conferences.php
+++ views/pages/conferences.php
@@ -10,16 +10,12 @@
           <p class="event__hour"><?php echo $event->hour->hour; ?></p>
           <div class="event__info">
             <h4 class="event__name"><?php echo $event->name; ?></h4>
-            <div>
-              <p class="event__info-description">
-                <?php echo $event->description; ?>
-              </p>
-            </div>
+            <p class="event__introduction"><?php echo $event->description; ?></p>
             <div class="event__autor-info">
               <picture>
                 <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $event->speaker->image;?>.webp" type="image/webp">
                 <source srcset="/img/speakers/<?php echo $event->speaker->image;?>.png" type="image/png">
-                <img src="<?php echo $_ENV['HOST'] . '/img/speakers/' . $event->speaker->image;?>.png" alt="Imagen Ponente">
+                <img class="event__image-autor" loading="lazy" width="200" height="300" src="<?php echo $_ENV['HOST'] . '/img/speakers/' . $event->speaker->image;?>.png" alt="Imagen Ponente">
               </picture>
 
               <p class="event__autor-name">
