ef25688259e33c36e292acb7d9d00eb60c37fd48
Mario Marcial <mariomarciallp@gmail.com>
2023-03-16 11:28:05 -0600

    758.- Creando las páginas públicas frontend- Creando la página de paquetes

--- views/pages/packs.php
+++ views/pages/packs.php
@@ -1,1 +1,37 @@
-<h2 class="dashboard__heading"><?php echo $title; ?></h2>
+<main class="packs">
+  <h2 class="packs__heading"><?php echo $title; ?></h2>
+  <p class="packs__description">
+    Compara los paquetes de DevWebCamp
+  </p>
+  <div class="packs__grid">
+    <div class="pack">
+      <h3 class="pack__name">Pase Gratis</h3>
+      <ul class="pack__list">
+        <li class="pack__list-item">Acceso Virtual a DevWebCamp</li>
+      </ul>
+      <p class="pack__price">$0</p>
+    </div>
+    <div class="pack">
+      <h3 class="pack__name">Pase Presencial</h3>
+      <ul class="pack__list">
+        <li class="pack__list-item">Acceso Presencial a DevWebCamp</li>
+        <li class="pack__list-item">Pase por 2 días</li>
+        <li class="pack__list-item">Acceso a Talleres y Conferencias</li>
+        <li class="pack__list-item">Acceso a las Grabaciones</li>
+        <li class="pack__list-item">Camisa del Evento</li>
+        <li class="pack__list-item">Comida y Bebida</li>
+      </ul>
+      <p class="pack__price">$199</p>
+    </div>
+    <div class="pack">
+      <h3 class="pack__name">Pase Virtual</h3>
+      <ul class="pack__list">
+        <li class="pack__list-item">Acceso Virtual a DevWebCamp</li>
+        <li class="pack__list-item">Pase por 2 días</li>
+        <li class="pack__list-item">Enlace a Talleres y Conferencias</li>
+        <li class="pack__list-item">Acceso a las Grabaciones</li>
+      </ul>
+      <p class="pack__price">$49</p>
+    </div>
+  </div>
+</main>
