6fb61cacd57d9c9a99e7675dfa013eb42dac1a81
Mario Marcial <mariomarciallp@gmail.com>
2023-03-18 20:13:16 -0600

    770.- Página principal de la conferencia - Aplicando CSS a los ponentes

--- views/pages/index.php
+++ views/pages/index.php
@@ -25,62 +25,64 @@
 <section class="speakers">
   <h2 class="speakers__heading">Speakers</h2>
   <p class="speakers__description">Conoce a nuestros expertos de DevWebCamp</p>
-  <?php foreach ($speakers as $speaker) { ?>
-    <div class="speaker">
-      <picture>
-        <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $speaker->image;?>.webp" type="image/webp">
-        <source srcset="/img/speakers/<?php echo $speaker->image;?>.png" type="image/png">
-        <img class="speaker__image" loading="lazy" width="200" height="300" src="<?php echo $_ENV['HOST'] . '/img/speakers/' . $speaker->image;?>.png" alt="Imagen Ponente">
-      </picture>
-      <div class="speaker__info">
-        <h4 class="speaker__name"><?php echo $speaker->name . " " . $speaker->lastname; ?></h4>
-        <p class="speaker__location"><?php echo $speaker->city . ", " . $speaker->country; ?></p>
-        <nav class="speaker__socials">
-          <?php $socials = json_decode($speaker->socials); ?>
-          <!-- Facebook -->
-          <?php if(!empty($socials->facebook)) { ?> 
-            <a class="speaker__link" rel="noopener noreferrer" target="_blank" href="<?php echo $socials->facebook; ?>">
-              <span class="speaker__hide">Facebook</span>
-            </a> 
-          <?php } ?>
-          <!-- Twitter -->
-          <?php if(!empty($socials->twitter)) { ?>
-            <a class="speaker__link" rel="noopener noreferrer" target="_blank" href="<?php echo $socials->twitter; ?>">
-              <span class="speaker__hide">Twitter</span>
-            </a>
-          <?php } ?> 
-          <!-- Youtube -->
-          <?php if(!empty($socials->twitter)) { ?>
-            <a class="speaker__link" rel="noopener noreferrer" target="_blank" href="<?php echo $socials->youtube; ?>">
-              <span class="speaker__hide">YouTube</span>
-            </a> 
-          <?php } ?>
-          <!-- Instagram -->
-          <?php if(!empty($socials->instagram)) { ?>
-            <a class="speaker__link" rel="noopener noreferrer" target="_blank" href="<?php echo $socials->instagram; ?>">
-              <span class="speaker__hide">Instagram</span>
-            </a> 
-          <?php } ?>
-          <!-- TikTok -->
-          <?php if(!empty($socials->tiktok)) { ?>
-            <a class="speaker__link" rel="noopener noreferrer" target="_blank" href="<?php echo $socials->tiktok;?>">
-              <span class="speaker__hide">Tiktok</span>
-            </a> 
-          <?php } ?>
-          <!-- GitHub -->
-          <?php if(!empty($socials->github)) { ?>
-            <a class="speaker__link" rel="noopener noreferrer" target="_blank" href="<?php echo $socials->github; ?>">
-              <span class="speaker__hide">GitHub</span>
-            </a>
-          <?php } ?>
-        </nav>
-        <ul class="speaker__skill-set">
-          <?php $tags = explode(',', $speaker->tags); ?>
-          <?php foreach ($tags as $tag) { ?>
-            <li class="speaker__skill"><?php echo $tag; ?></li>
-          <?php } ?>
-        </ul>
+  <div class="speakers__grid">
+    <?php foreach ($speakers as $speaker) { ?>
+      <div class="speaker">
+        <picture>
+          <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $speaker->image;?>.webp" type="image/webp">
+          <source srcset="/img/speakers/<?php echo $speaker->image;?>.png" type="image/png">
+          <img class="speaker__image" loading="lazy" width="200" height="300" src="<?php echo $_ENV['HOST'] . '/img/speakers/' . $speaker->image;?>.png" alt="Imagen Ponente">
+        </picture>
+        <div class="speaker__info">
+          <h4 class="speaker__name"><?php echo $speaker->name . " " . $speaker->lastname; ?></h4>
+          <p class="speaker__location"><?php echo $speaker->city . ", " . $speaker->country; ?></p>
+          <nav class="speaker-socials">
+            <?php $socials = json_decode($speaker->socials); ?>
+            <!-- Facebook -->
+            <?php if(!empty($socials->facebook)) { ?> 
+              <a class="speaker-socials__link" rel="noopener noreferrer" target="_blank" href="<?php echo $socials->facebook; ?>">
+                <span class="speaker-socials__hide">Facebook</span>
+              </a> 
+            <?php } ?>
+            <!-- Twitter -->
+            <?php if(!empty($socials->twitter)) { ?>
+              <a class="speaker-socials__link" rel="noopener noreferrer" target="_blank" href="<?php echo $socials->twitter; ?>">
+                <span class="speaker-socials__hide">Twitter</span>
+              </a>
+            <?php } ?> 
+            <!-- Youtube -->
+            <?php if(!empty($socials->youtube)) { ?>
+              <a class="speaker-socials__link" rel="noopener noreferrer" target="_blank" href="<?php echo $socials->youtube; ?>">
+                <span class="speaker-socials__hide">YouTube</span>
+              </a> 
+            <?php } ?>
+            <!-- Instagram -->
+            <?php if(!empty($socials->instagram)) { ?>
+              <a class="speaker-socials__link" rel="noopener noreferrer" target="_blank" href="<?php echo $socials->instagram; ?>">
+                <span class="speaker-socials__hide">Instagram</span>
+              </a> 
+            <?php } ?>
+            <!-- TikTok -->
+            <?php if(!empty($socials->tiktok)) { ?>
+              <a class="speaker-socials__link" rel="noopener noreferrer" target="_blank" href="<?php echo $socials->tiktok;?>">
+                <span class="speaker-socials__hide">Tiktok</span>
+              </a> 
+            <?php } ?>
+            <!-- GitHub -->
+            <?php if(!empty($socials->github)) { ?>
+              <a class="speaker-socials__link" rel="noopener noreferrer" target="_blank" href="<?php echo $socials->github; ?>">
+                <span class="speaker-socials__hide">GitHub</span>
+              </a>
+            <?php } ?>
+          </nav>
+          <ul class="speaker__skill-set">
+            <?php $tags = explode(',', $speaker->tags); ?>
+            <?php foreach ($tags as $tag) { ?>
+              <li class="speaker__skill"><?php echo $tag; ?></li>
+            <?php } ?>
+          </ul>
+        </div>
       </div>
-    </div>
-  <?php } ?>
+    <?php } ?>
+  </div>
 </section>
