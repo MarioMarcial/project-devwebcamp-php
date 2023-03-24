4f8e5b257c7b086b4d47698f102d86b44eeaf56a
Mario Marcial <mariomarciallp@gmail.com>
2023-03-24 12:30:33 -0600

    794.- Ajustes finales al admin - Listando y paginando registros

--- controllers/RegisteredController.php
+++ controllers/RegisteredController.php
@@ -1,12 +1,38 @@
 <?php
 namespace Controllers;
+
+use Model\Pack;
+use Model\User;
 use MVC\Router;
+use Model\Register;
+use Classes\Pagination;
 
 class RegisteredController {
   public static function index(Router $router) {
-    
+    if(!is_admin()) {
+      header('Location: /');
+    }
+    // Pagination
+    $current_page = filter_var($_GET['page'], FILTER_VALIDATE_INT);
+    if(!$current_page || $current_page < 1) {
+      header('Location: /admin/registrados?page=1');
+    }
+    $records_per_page = 10;
+    $total_records = Register::total();
+    $pagination = new Pagination($current_page, $records_per_page, $total_records);
+    if($current_page > $pagination->total_pages()) {
+      header('Location: /admin/registrados?page=1');
+    }
+    // Get registrations
+    $registrations = Register::paginate($records_per_page, $pagination->offset());
+    foreach ($registrations as $registration) {
+      $registration->user = User::find($registration->user_id);
+      $registration->pack = Pack::find($registration->pack_id);
+    }
     $router->render('admin/registered/index', [
-      'title' => 'Usuarios Registrados'
+      'title' => 'Usuarios Registrados',
+      'registrations' => $registrations,
+      'pagination' => $pagination->pagination()
     ]);
   }
 }
--- views/admin/registered/index.php
+++ views/admin/registered/index.php
@@ -1,1 +1,34 @@
 <h2 class="dashboard__heading"><?php echo $title; ?></h2>
+<div class="dashboard__container">
+  <?php if(!empty($registrations)) { ?>
+    <table class="table">
+      <thead class="table__head">
+        <tr class="table__tr">
+          <th scope="col" class="table__th">Nombre</th>
+          <th scope="col" class="table__th">Email</th>
+          <th scope="col" class="table__th">Plan</th>
+        </tr>
+      </thead>
+      <tbody class="table__tbody">
+        <?php foreach ($registrations as $registration) { ?>
+          <tr class="table__tr">
+            <td class="table__td">
+                <?php echo $registration->user->name . " " . $registration->user->lastname;?>
+            </td>
+            <td class="table__td">
+              <?php echo $registration->user->email;?>
+            </td>
+            <td class="table__td">
+              <?php echo $registration->pack->name;?>
+            </td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  <?php } else { ?>
+    <p class="text-center">No hay Registros Aún</p>
+  <?php } ?>
+</div>
+<?php
+  echo $pagination;
+?>
